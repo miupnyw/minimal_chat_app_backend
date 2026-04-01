1985b7ca4572a189012a7f047347afd8f87559cf
Miu <miu@gmail.com>
2026-04-02 00:22:42 +0700

    [-] comment socket server

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
 const http = require('http');
 const app = require('./app');
-const initSocket = require('./socket');
+//const initSocket = require('./socket');
 const { PORT } = require('./config');
 
 const server = http.createServer(app);
 
-initSocket(server);
+//initSocket(server);
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);

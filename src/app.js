9f447861cbdc08afcb7f5205e79d1af734a0bf7d
Miu <miu@gmail.com>
2026-04-07 22:14:55 +0700

    [+] add dockerfile

--- src/app.js
+++ src/app.js
@@ -6,8 +6,8 @@ const routes = require('./routes');
 const app = express();
 
 const allowedOrigins = [
-	'http://localhost:5173',
-	'http://127.0.0.1:5173'
+	'http://localhost:8080',
+	'http://127.0.0.1:8080'
 ]
 
 app.use(cors({
